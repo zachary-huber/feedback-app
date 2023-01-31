938e4db6aeb7735d988f44dc99b7cb36b323f869
Genevieve Couture <eve.iveneg11235@gmail.com>
2023-01-31 17:28:01 -0600

    sets up MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Button variant="contained">MUI component</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
