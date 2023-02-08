6bf5d0bcfb620e5fc30d809eaa4bb96addf6dcd4
Genevieve Couture <eve.iveneg11235@gmail.com>
2023-02-08 15:21:31 -0600

    Sets up paging

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
