d1d200892ed3e4909975dd8e04496af82bd9bc6a
GenevieveCouture23 <122845407+GenevieveCouture23@users.noreply.github.com>
2023-02-13 18:18:52 -0600

    Update server.php

--- PHP/server.php
+++ PHP/server.php
@@ -1,5 +1,6 @@
 <?php
 //this folder and file is temporary 
+//to run the server locally go into the PHP file in the console and run the command php -S localhost:8000
     header('Access-Control-Allow-Origin: http://localhost:3000');
     $user = $_POST['name'];
     echo ("Hello from server: $user");
