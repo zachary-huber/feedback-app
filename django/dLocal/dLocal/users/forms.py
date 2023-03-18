e2e9936d49a0934138cb371e853204a73176d592
zachary-huber <zachuber.zh@gmail.com>
2023-03-17 21:25:44 -0600

    fixed the ugly signup fields

--- django/dLocal/dLocal/users/forms.py
+++ django/dLocal/dLocal/users/forms.py
@@ -3,15 +3,26 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    # user = User.CharField(max_length=50, min_length=4, required=True,
+    #                        help_text='My help text', label='My Label',
+    #                        widget=User.TextInput(attrs={'class': 'form-control', 
+    #                        'placeholder': 'User'}))
+
     class Meta:
         model = User
         fields = ['first_name', 'email', 'username', 'password1', 'password2']
         labels = {
             'first_name':'Name',
         }
+        
+        
 
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Password confirmation'
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
