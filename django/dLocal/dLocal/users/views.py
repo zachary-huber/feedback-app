b4f368aea960221a1d5000a35dfea589c11fd533
zachary-huber <zachuber.zh@gmail.com>
2023-03-18 19:29:31 -0600

    fixed user redirects

--- django/dLocal/dLocal/users/views.py
+++ django/dLocal/dLocal/users/views.py
@@ -42,23 +42,26 @@ def logoutUser(request):
     return redirect('login')
 
 def registerUser(request):
-    page = 'register'
-    form = CustomUserCreationForm()
+    if (request.user.is_authenticated):
+        return redirect('profiles')
+    else:
+        page = 'register'
+        form = CustomUserCreationForm()
 
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.username = user.username.lower()
-            user.save()
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.username = user.username.lower()
+                user.save()
 
-            messages.success(request, 'User account was created!')
+                messages.success(request, 'User account was created!')
 
-            login(request, user)
-            return redirect('profiles')
-        
-        else:
-            messages.error(request, 'An error has occurred!')
+                login(request, user)
+                return redirect('profiles')
+            
+            else:
+                messages.error(request, 'An error has occurred!')
 
 
     context = {'page':page, 'form':form}
@@ -66,32 +69,38 @@ def registerUser(request):
 
 
 def user_forms(request, idd):
-    # Retrieve the user object based on the user_id
-    user_id = User.objects.get(id=idd)
+    if not (request.user.is_authenticated):
+        return redirect('login')
+    else:
+        # Retrieve the user object based on the user_id
+        user_id = User.objects.get(id=idd)
 
-    # Retrieve all forms associated with the user
-    user_forms = Forms.objects.filter(user_id=idd)
+        # Retrieve all forms associated with the user
+        user_forms = Forms.objects.filter(user_id=idd)
 
-    form_ids = []
-    form_titles = []
-    for user_form in user_forms:
-        form_ids.append(user_form.form_id)
-        form_titles.append(user_form.title)
+        form_ids = []
+        form_titles = []
+        for user_form in user_forms:
+            form_ids.append(user_form.form_id)
+            form_titles.append(user_form.title)
+            
+        forms = zip(form_ids, form_titles)
+        print(forms)
+        # get form title from user_forms
         
-    forms = zip(form_ids, form_titles)
-    print(forms)
-    # get form title from user_forms
-    
-    # passing the user_id, user_forms, form_ids, and form_titles to the profiles.html template
-    return render(request, 'users/profiles.html',  {'id': user_id, 'forms': forms})
+        # passing the user_id, user_forms, form_ids, and form_titles to the profiles.html template
+        return render(request, 'users/profiles.html',  {'id': user_id, 'forms': forms})
 
 
 def profiles(request):
-    profiles = Profile.objects.all()
-    context = {'profiles':profiles}
-    user_id = request.user.id
-    
-    return user_forms(request, user_id)
+    if not (request.user.is_authenticated):
+        return redirect('login')
+    else:
+        profiles = Profile.objects.all()
+        context = {'profiles':profiles}
+        user_id = request.user.id
+        
+        return user_forms(request, user_id)
     
     return render(request, 'users/profiles.html', context)
 
